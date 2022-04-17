879c36465ee477379ccc92cfacc8c264f64d01ca
samschool789@gmail.com <samschool789@gmail.com>
2022-04-16 21:30:02 -0500

    time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,8 @@
 //display current time at the top of the page
-$("#currentDay").html(moment().format("MMMM Do, YYYY"));
-
-//have time blocks for standard business hours
+$("#currentDay").html(moment().format("[Today's Date:] MMMM Do, YYYY"));
 
 
-//have a way to view the time blocks
 
-//color code them using bootstrap for past, present, future
-
-//add eventlistener to click on the time block
-
-//be able to enter an event in the time block
-
-//be able to save the time block and semd to local storage
 
 
 
